7d34b1a11efd8c49d1e9f2658a16f13f2d1d7af3
Yushika06 <yuukionime06@gmail.com>
2024-07-30 11:30:36 +0700

    add models

---
+++ app/Models/User copy.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Auth\MustVerifyEmail;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class User extends Authenticatable
+{
+    use MustVerifyEmail, HasFactory, Notifiable;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'name',
+        'email',
+        'password',
+        'google_id',
+        'google_token',
+        'google_refresh_token',
+        'whatsapp_number',
+        'profile_picture',
+        'company_id',
+        'position_id',
+        'address_details_id',
+        'role',
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array<int, string>
+     */
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array<string, string>
+     */
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+    ];
+    public function getProfilePhotoUrlAttribute()
+    {
+        return $this->profile_photo_path
+            ? asset('storage/' . $this->profile_photo_path)
+            : 'https://ui-avatars.com/api/?name=' . urlencode($this->name) . '&color=7F9CF5&background=EBF4FF';
+    }
+    public function position()
+    {
+        return $this->belongsTo(Position::class);
+    }
+
+    public function company()
+    {
+        return $this->belongsTo(Company::class);
+    }
+    public function addressDetail()
+    {
+        return $this->belongsTo(AddressDetail::class);
+    }
+}
